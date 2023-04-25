ca451efee507cf5d8fbe10b4e0a4ad9e346e58a0
Talal <talal2ramadan.com>
2023-04-25 09:30:34 -0700

    h

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,33 @@
-int main(void)
+#include "main.h"
+/**
+ * times_table:
+ * a function can print the 9 times table,
+ *		starting with zero
+ */
+void times_table(void)
 {
+	int num2;
+	int m5;
+	int pt;
+
+	for (num2 = 0; num2 <= 9; num2++)
+	{
+		_putchar('0');
+
+		for (m5 = 1; m5 <= 9; m5++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			pt = num2 * m5;
+
+			if (pt <= 9)
+				_putchar(' ');
+			else
+				_putchar((pt / 10) + '0');
+
+			_putchar((pt % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
