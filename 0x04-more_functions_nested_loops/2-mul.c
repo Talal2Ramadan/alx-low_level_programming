3699d078954aa18b4395790644800018b734a8b5
Talal <talal2ramadan.com>
2023-04-25 14:41:57 -0700

    1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul - getting the multiplication of two inputs.
+ *@a: 1st input
+ *@b: 2nd input
+ * Return: the multiplication of two numbers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * printing_numbers - display numbers from 0 to 9.
+ * using _putchar
+ * Return: Always 0 (Success).
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
+
